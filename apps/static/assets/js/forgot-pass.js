04ca21cdc21b7c112dedf9d643343c0e7253a683
Merciditas Gelvez <merciditasmayvgelvez@iskolarngbayan.pup.edu.ph>
2024-05-30 21:01:10 +0800

    BABU-56 fix: login and register bugs

--- apps/static/assets/js/forgot-pass.js
+++ apps/static/assets/js/forgot-pass.js
@@ -1,13 +1,45 @@
 //-----------EMAIL VALIDATION--------//
 $(document).ready(function () {
-  $("#email").on("submit", function (event) {
-    event.preventDefault(); // Prevent the form from being submitted normally
-    var email = $("#email").val();
-    $.post("/forgot_password", { email: email }, function (data) {
-      $(".email-error-message")
-        .text("If an account with that email address exists, we’ve sent a password reset link.")
-        .addClass("success-message");
-    });
+  $("#email").on("input", function () {
+    var input = $(this);
+    var email = input.val();
+    var errorMessageContainer = $(".email-error-message");
+
+    // Regular expression pattern for email validation
+    var emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    // Check for .c0m or .c00m instead of .com
+    var invalidDomainPattern = /\.(c0m|c00m)$/i;
+
+    // Check if email contains @test
+    var testEmailPattern = /@test/i;
+
+    // Check if email contains space
+    if (/\s/.test(email)) {
+      input.removeClass("valid").addClass("invalid");
+      errorMessageContainer
+        .text("Email cannot contain spaces")
+        .addClass("error-message")
+        .removeClass("success-message");
+    } else if (testEmailPattern.test(email)) {
+      input.removeClass("valid").addClass("invalid");
+      errorMessageContainer
+        .text("Email cannot contain @test")
+        .addClass("error-message")
+        .removeClass("success-message");
+    } else if (invalidDomainPattern.test(email)) {
+      input.removeClass("valid").addClass("invalid");
+      errorMessageContainer
+        .text("Invalid email format. Please use .com instead of .c0m")
+        .addClass("error-message")
+        .removeClass("success-message");
+    } else {
+      input.removeClass("valid").addClass("invalid");
+      errorMessageContainer
+        .text("Email is required")
+        .addClass("error-message")
+        .removeClass("success-message");
+    }
   });
 });
 
