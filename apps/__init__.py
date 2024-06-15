ac922a7b8cee8660627fb6584e9355f868a8bc4d
Merciditas Gelvez <merciditasmayvgelvez@iskolarngbayan.pup.edu.ph>
2024-06-15 17:05:32 +0800

    feat(database): add new table for history

--- apps/__init__.py
+++ apps/__init__.py
@@ -9,46 +9,35 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
+from flask_migrate import Migrate
 from flask_mail import Mail
-
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 mail = Mail()
+migrate = Migrate()
 
 def register_extensions(app):
     db.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     mail.init_app(app)
-
 
 def register_blueprints(app):
     for module_name in ('authentication', 'home'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
-
 def configure_database(app):
+    from apps.models import ChatHistory  # Import the ChatHistory model
 
     @app.before_first_request
     def initialize_database():
-        try:
-            db.create_all()
-        except Exception as e:
-
-             print('> Error: DBMS Exception: ' + str(e) )
-
-            # # fallback to SQLite
-            # basedir = os.path.abspath(os.path.dirname(__file__))
-            # app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-
-            # print('> Fallback to SQLite ')
-            # db.create_all()
+        db.create_all()
 
     @app.teardown_request
     def shutdown_session(exception=None):
         db.session.remove()
-
 
 def create_app(config):
     app = Flask(__name__)
@@ -56,5 +45,4 @@ def create_app(config):
     register_extensions(app)
     register_blueprints(app)
     configure_database(app)
-    mail = Mail(app)
     return app
